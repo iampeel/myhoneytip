02559cb3c817f56cca5bbb989c7431752924b7a9
moonpil cheon <iampeel@naver.com>
2022-01-19 16:16:49 +0900

    json 데이터 이용

--- pages/MainPage.js
+++ pages/MainPage.js
@@ -8,10 +8,14 @@ import {
   TouchableOpacity,
   ScrollView,
 } from "react-native";
+import data from "../data.json";
 
 export default function MainPage() {
   // 앱 실행시 경고창 무시
   console.disableYellowBox = true;
+
+  // 데이터 변수
+  let tip = data.tip;
 
   // 날씨 변수
   let todayWeather = 27;
@@ -49,30 +53,28 @@ export default function MainPage() {
 
       {/* 카드 여러 개 묶음 */}
       <View style={styles.cardContainer}>
-        {/* 카드 하나 묶음 */}
-        <View style={styles.card}>
-          {/* 카드 이미지 */}
-          <Image
-            style={styles.cardImage}
-            source={{
-              uri: "https://firebasestorage.googleapis.com/v0/b/sparta-image.appspot.com/o/lecture%2Fpizza.png?alt=media&token=1a099927-d818-45d4-b48a-7906fd0d2ad3",
-            }}
-          />
+        {tip.map((content, i) => {
+          return (
+            // 카드 하나 묶음
+            <View style={styles.card} key={i}>
+              {/* 카드 이미지 */}
+              <Image style={styles.cardImage} source={{ uri: content.image }} />
 
-          {/* 카드 텍스트 묶음 */}
-          <View style={styles.cardText}>
-            <Text style={styles.cardTitle}>먹다 남은 피자를 촉촉하게!</Text>
-            <Text style={styles.cardDesc} numberOfLines={3}>
-              먹다 남은 피자는 수분이 날라가기 때문에 처음처럼 맛있게 먹을 수
-              없는데요. 이럴 경우 그릇에 물을 받아 전자레인지 안에서 1분
-              30초에서 2분 정도 함께 돌려주면 촉촉하게 먹을 수 있습니다. 물이
-              전자레인지 안에서 수증기를 일으키고, 피자에 촉촉함을 더해줍니다.
-            </Text>
-            <Text style={styles.cardDate}>2020.09.09</Text>
-          </View>
-          {/* // 카드 텍스트 묶음 */}
-        </View>
-        {/* // 카드 하나 묶음 */}
+              {/* 카드 텍스트 묶음 */}
+              <View style={styles.cardText}>
+                <Text style={styles.cardTitle} numberOfLines={1}>
+                  {content.title}
+                </Text>
+                <Text style={styles.cardDesc} numberOfLines={3}>
+                  {content.desc}
+                </Text>
+                <Text style={styles.cardDate}>{content.date}</Text>
+              </View>
+              {/* // 카드 텍스트 묶음 */}
+            </View>
+            // //카드 하나 묶음
+          );
+        })}
       </View>
       {/* // 카드 여러 개 묶음 */}
     </ScrollView>
