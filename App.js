55295b7c24944fba092b45a70174da5ec5373702
moonpil cheon <iampeel@naver.com>
2022-01-19 16:29:10 +0900

    AboutPage 구현

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 import MainPage from "./pages/MainPage";
+import AboutPage from "./pages/AboutPage";
 
 export default function App() {
-  return <MainPage />;
+  return <AboutPage />;
+  // return <MainPage />;
 }
