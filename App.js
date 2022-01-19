f93d075fed121c37ad44b82533265d29a83726f4
moonpil cheon <iampeel@naver.com>
2022-01-19 16:44:47 +0900

    DetailPage 화면 세팅

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
 import MainPage from "./pages/MainPage";
 import AboutPage from "./pages/AboutPage";
+import DetailPage from "./pages/DetailPage";
 
 export default function App() {
-  return <AboutPage />;
+  return <DetailPage />;
+  // return <AboutPage />;
   // return <MainPage />;
 }
