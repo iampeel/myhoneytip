f260d40b2f2e40b09de5d44c76a877dd6fbd1f7f
moonpil cheon <iampeel@naver.com>
2022-01-20 14:20:59 +0900

    버튼에 따른 카드 구성

--- pages/MainPage.js
+++ pages/MainPage.js
@@ -17,6 +17,9 @@ export default function MainPage() {
   console.disableYellowBox = true;
 
   const [state, setState] = useState([]);
+  // DB에서 가져온 원본은 state에 담고
+  // 여기엔 버튼에 따른 내용을 담는다
+  const [cateState, setCateState] = useState([]);
   const [ready, setReady] = useState(true);
 
   // 데이터 변수
@@ -28,6 +31,7 @@ export default function MainPage() {
     setTimeout(() => {
       setState(tip);
       setReady(false);
+      setCateState(tip);
     }, 2000);
 
     // []를 안 쓰면 렌더링 될 때마다 이게 실행됨
@@ -38,6 +42,22 @@ export default function MainPage() {
   // 날씨 변수
   let todayWeather = 27;
   let todayCondition = "흐림";
+
+  const category = (cate) => {
+    if (cate == "전체보기") {
+      setCateState(state);
+    } else {
+      // filetr()라는 함수는 state에 담겨있는 내용을 차례로 가져오는 기능
+      // d는 인자를 받아오려고 쓴 키워드
+      // 가져온 항목의 category값이 "생활"인 것만 리턴시켜서
+      // cateState 변수에 넣음
+      setCateState(
+        state.filter((d) => {
+          return d.category == cate;
+        })
+      );
+    }
+  };
 
   return ready ? (
     <Loading />
@@ -57,24 +77,45 @@ export default function MainPage() {
 
       {/* 중간 버튼 묶음 */}
       <ScrollView style={styles.middleContainer} horizontal>
-        <TouchableOpacity style={styles.middleButton01}>
+        <TouchableOpacity
+          style={styles.middleButtonAll}
+          onPress={() => {
+            category("전체보기");
+          }}
+        >
+          <Text style={styles.middleButtonTextAll}>전체보기</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={styles.middleButton01}
+          onPress={() => {
+            category("생활");
+          }}
+        >
           <Text style={styles.middleButtonText}>생활</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.middleButton02}>
+        <TouchableOpacity
+          style={styles.middleButton02}
+          onPress={() => {
+            category("재테크");
+          }}
+        >
           <Text style={styles.middleButtonText}>재테크</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.middleButton03}>
+        <TouchableOpacity
+          style={styles.middleButton03}
+          onPress={() => {
+            category("반려견");
+          }}
+        >
           <Text style={styles.middleButtonText}>반려견</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.middleButton04}>
-          <Text style={styles.middleButtonText}>꿀팁 찜</Text>
         </TouchableOpacity>
       </ScrollView>
 
       {/* 카드 여러 개 묶음 */}
       <View style={styles.cardContainer}>
-        {/* tip이 아닌 state로 변경 */}
-        {state.map((content, i) => {
+        {/* tip --> state --> cateState */}
+        {cateState.map((content, i) => {
           // Card.js로 보냄
           return <Card content={content} key={i} />;
         })}
@@ -120,6 +161,16 @@ const styles = StyleSheet.create({
     height: 60,
   },
 
+  // 전체보기 추가
+  middleButtonAll: {
+    width: 100,
+    height: 50,
+    padding: 15,
+    backgroundColor: "#20b2aa",
+    borderRadius: 15,
+    margin: 7,
+  },
+
   // 각각의 중간 버튼
   middleButton01: {
     width: 100,
@@ -157,6 +208,13 @@ const styles = StyleSheet.create({
 
   // 중간 버튼 텍스트
   middleButtonText: {
+    color: "#fff",
+    fontWeight: "700",
+    textAlign: "center",
+  },
+
+  // 전체보기 추가
+  middleButtonTextAll: {
     color: "#fff",
     fontWeight: "700",
     textAlign: "center",
